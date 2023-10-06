c05376b4d9661bb3530706fb20558f7584acf394
cronnysousa <carlos.ronny@gmail.com>
2023-10-06 11:42:54 -0300

    Leiaute principal

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import LivroLista from './LivroLista';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      
+
       <StatusBar style="auto" />
-    </View>
+      
+      <LivroLista></LivroLista>
+     
+      
+    </SafeAreaView>
   );
 }
 
---
+++ LivroLista.tsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import { View, FlatList,StyleSheet } from 'react-native';
+import axios from 'axios';
+import { Button,Text, Card, Avatar } from 'react-native-paper';
+
+export default function LivroLista() {
+  const [livros, setLivros] = useState([]);
+  const LeftContent = props => <Avatar.Icon {...props} icon="folder" />
+  useEffect(() => {
+    // Fazendo uma solicitação GET para a API de livros
+    axios.get('https://bibliotecaetecmaua.azurewebsites.net/api/LivrosSedeApi')
+      .then(response => {
+        setLivros(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []); // O array vazio faz com que o useEffect seja executado apenas uma vez, após a renderização inicial
+
+  return (
+    <View>
+      <Text>Lista de Livros:</Text>
+      <FlatList
+        data={livros}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem=
+        {
+            ({ item }) => 
+        
+            <Card mode='elevated' style={estilocard.container}  >
+            <Card.Title title={item.autorPrincipal}   />
+            <Card.Content>
+              <Text variant="titleLarge">{item.titulo}</Text>
+              <Text variant="bodyMedium">{}</Text>
+            </Card.Content>
+            <Card.Cover  source={{ uri: `https://bibliotecaetecmaua.azurewebsites.net/Content/Images/${item.imagem}` }} />
+
+          </Card>
+         } // Supondo que cada livro tenha uma propriedade 'titulo'
+      />
+    </View>
+  );
+};
+
+
+const estilocard = StyleSheet.create({
+    container: {
+      margin: 10,
+    },
+  });
